f69d06ce1b36527aa9e4ce1bb87a8044a0b1a864
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-07-10 23:55:26 +0700

    add explaination as comment

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,13 +2,16 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Box, Button, Container, HStack } from 'native-base'
 
+// เรียกใช้ props ชื่อ navigation สิ่งนี้ได้จาก NativeBaseProvider
 const HomePage = ({ navigation }) => {
   return (
     <>
       <Container style={{ padding: 10 }}>
+        {/* เรียกใช้ navigation ในการเปิดไปยัง Screen ที่ชื่อว่า Chat */}
         <Button onPress={() => navigation.navigate('Chat')}  style={{ marginBottom: 10 }} >
           <Text style={{ color: 'white' }} >Chat</Text>
         </Button>
+        {/* เรียกใช้ navigation ในการเปิดไปยัง Screen ที่ชื่อว่า Settings */}
         <Button onPress={() => navigation.navigate('Settings')}>
           <Text style={{ color: 'white' }} >Setting</Text>
         </Button>
