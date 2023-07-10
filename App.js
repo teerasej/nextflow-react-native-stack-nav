3afca68f44e1143741f559d5178e5cf26527614e
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-07-10 23:56:20 +0700

    clear

--- App.js
+++ App.js
@@ -9,27 +9,18 @@ import SettingPage from './pages/SettingPage'
 
 
 // import NavigationContainer 
-import { NavigationContainer } from '@react-navigation/native';
+
 // import function createNativeStackNavigator ในการสร้าง Stack Navigation
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 // สร้าง Stack component 
-const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
     <NativeBaseProvider>
       {/* setup  NavigationContainer */}
-      <NavigationContainer>
-        {/* สร้าง Stack Navigator  */}
-        <Stack.Navigator>
-          {/* สร้าง กำหนด Home Component ให้กับหน้าแอพ (Screen ที่ชื่อว่า Home  */}
-          <Stack.Screen name="Home" component={HomePage} />
-          <Stack.Screen name="Chat" component={ChatPage} />
-          <Stack.Screen name="Settings" component={SettingPage} />
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-      </NavigationContainer>
+      
     </NativeBaseProvider>
   );
 }
